3bf697a1451d27834afbfa81e364756b2ff5c873
Tkadha(LAPTOP) <emforhs228@naver.com>
2023-09-28 20:00:08 +0900

    made mouse_cursor

--- Drill06.py
+++ Drill06.py
@@ -11,10 +11,13 @@ hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
+    global x,y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -22,6 +25,14 @@ def handle_events():
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+hide_cursor()
 
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    hand.draw(x,y)
+    update_canvas()
+
+    handle_events()
 
 close_canvas()
