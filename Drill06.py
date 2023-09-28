6c13c3e39a1dcf6f41cb76ffb6eb98d19b026a50
Tkadha(LAPTOP) <emforhs228@naver.com>
2023-09-28 20:44:01 +0900

    Drill06 제출(2020182042 최준하)
    최종 수정본

--- Drill06.py
+++ Drill06.py
@@ -17,6 +17,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             Move = False
+            idle = False
+            running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
@@ -27,6 +29,8 @@ def handle_events():
             running = True
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             Move = False
+            idle = False
+            running = False
 
 
 point_list = []
@@ -52,7 +56,6 @@ while Move:
         x1, y1 = char_x, char_y
         x2, y2 = point_list[0], point_list[1]
         for i in range(0, 100 + 1, 3):
-            handle_events()
             t = i / 100
             char_x = (1 - t) * x1 + t * x2
             char_y = (1 - t) * y1 + t * y2
@@ -60,18 +63,20 @@ while Move:
             TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
             hand.draw(x, y)
             if 0 > x2 - x1:
-                char_motion=0
+                char_motion = 0
             elif 0 <= x2 - x1:
-                char_motion=1
+                char_motion = 1
             character.clip_draw(frame * 100, 100 * char_motion, 100, 100, char_x, char_y)
-            frame = (frame+1) % 8
+            frame = (frame + 1) % 8
             for j in range(0, len(point_list), 2):
                 hand.draw(point_list[j], point_list[j + 1])
             update_canvas()
+            handle_events()
+            if Move == False and idle == False and running == False:
+                close_canvas()
             delay(0.05)
         del point_list[1], point_list[0]
         if len(point_list) == 0:
             running = False
             idle = True
-
 close_canvas()
