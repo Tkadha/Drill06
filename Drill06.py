a4a89d39b65f21afe9a804ab363d04f3ced87ed9
Tkadha(LAPTOP) <emforhs228@naver.com>
2023-09-28 20:18:48 +0900

    mouse click

--- Drill06.py
+++ Drill06.py
@@ -11,16 +11,23 @@ hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x,y
+    global x, y
+    global point_list
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            point_list.append(x)
+            point_list.append(y)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+point_list = []
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
@@ -30,7 +37,9 @@ hide_cursor()
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    hand.draw(x,y)
+    hand.draw(x, y)
+    for i in range (0,len(point_list),2):
+        hand.draw(point_list[i],point_list[i+1])
     update_canvas()
 
     handle_events()
